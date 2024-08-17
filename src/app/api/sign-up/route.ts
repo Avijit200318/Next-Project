449ee6c04f07436dddf5dc0bcb6f79531764363d
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-08-17 13:53:52 +0530

    debug some errors to sign-up and veify-code api routes

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -40,7 +40,7 @@ export async function POST(request: Request) {
                 email,
                 password: hashPassword,
                 verifyCode,
-                veifyCodeExpiry: expiryDate,
+                verifyCodeExpiry: expiryDate,
                 isVerified: false,
                 isAcceptingMessage: true,
                 message: []
