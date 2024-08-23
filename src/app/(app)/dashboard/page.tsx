64d5681b08240bb6cb342e04ae13bf4a58373892
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-08-23 13:03:15 +0530

    add sign out button to the dashboard page

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,10 +1,16 @@
 "use client";
 import React from 'react'
+import { signOut } from "next-auth/react"
+
 
 export default function page() {
-  return (
-    <div>
-      this is dashboard page
-    </div>
-  )
+
+    return (
+        <div>
+            <h1 className="my-4">This is the dashboard page</h1>
+
+            {/* in next auth there is an inbuild signout function. We have to use it. Instead of doing it maually */}
+            <button onClick={() => signOut()}>Sign out</button>
+        </div>
+    )
 }
